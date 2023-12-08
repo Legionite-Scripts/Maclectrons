751afcee7e3df94aa8fc89607a767351e7f50f27
Legionite-Scripts <samuelonwuka88@gmail.com>
2023-12-07 18:37:17 -0800

    done

--- src/Components/Deals.jsx
+++ src/Components/Deals.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import tvImage from "./Images/dealsTV.png";
 
 export const Deals = () => {
   return (
@@ -19,7 +20,30 @@ export const Deals = () => {
         <a href="#">Huawei</a>
         <a href="#">Pizzie</a>
       </Container>
-      <Container id="dealsProducts"></Container>
+      <Container id="dealsProducts">
+        <Container fluid id="topDealsDiv">
+          <h6>
+            Brand new Smart TV starts at just <span>$800</span>
+          </h6>
+        </Container>
+        <Container id="middleDealsDiv">
+          <Container id="middleDealsText">
+            <h6 style={{ fontWeight: 700, color: "#1e1e1e" }}>
+              HDMI 1920X1080
+            </h6>
+            <h1 id="normalDealsText">Super Smart HD TV</h1>
+            <h1 id="boldDealsText">
+              <span>
+                <i className="fa-solid fa-scissors"></i> Flat 50%
+              </span>{" "}
+              off on $100 purchases
+            </h1>
+          </Container>
+          <Container id="middleDealsImage">
+            <img src={tvImage} alt="Product" />
+          </Container>
+        </Container>
+      </Container>
     </Container>
   );
 };
