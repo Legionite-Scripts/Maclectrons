ed303bf989046f7feb3818aa2ce184a213a865b5
Legionite-Scripts <samuelonwuka88@gmail.com>
2023-12-11 09:30:36 -0800

    done

--- src/Components/PopularViews.jsx
+++ src/Components/PopularViews.jsx
@@ -4,6 +4,10 @@ import earbudsImage from "./Images/earbuds.png";
 import oraimoImage from "./Images/oraimo.png";
 import laptopImage from "./Images/laptop.png";
 import redHeadphonesImage from "./Images/redHeadphones.png";
+import earphonesImage from "./Images/earphone.png";
+import clockImage from "./Images/clock.png";
+import clipperImage from "./Images/clipper.png";
+import fridgeImage from "./Images/fridge.png";
 
 export const PopularViews = () => {
   return (
@@ -106,10 +110,15 @@ export const PopularViews = () => {
       <Container fluid id="popularProducts">
         {/* First Item */}
         <Container id="item">
-          <img src={earbudsImage} alt="Buds" />
+          <Container fluid id="freeDeliveryDiv">
+            <Container id="innerFreeDelivery">
+              <h6>Free Delivery</h6>
+            </Container>
+          </Container>
+          <img src={earphonesImage} alt="Earphones" />
           <Container fluid id="itemText">
-            <p>In stock</p>
-            <h6>Philips Hair Blow Dryer 605WTZ</h6>
+            <p>Few Units Left</p>
+            <h6>UltraSound Samsung Earphones</h6>
           </Container>
           <Container fluid id="review">
             <Container id="star">
@@ -119,18 +128,18 @@ export const PopularViews = () => {
               <i className="fa-solid fa-star"></i>
               <i className="fa-solid fa-star"></i>
             </Container>
-            <p>86 reviews</p>
+            <p>600 reviews</p>
           </Container>
           <Container id="popularPriceDiv">
-            <h3>$87.99</h3>
+            <h3>$3.50</h3>
           </Container>
         </Container>
         {/* Second Item */}
         <Container id="item">
-          <img src={oraimoImage} alt="mp4" />
+          <img src={clockImage} alt="LED Clock" />
           <Container fluid id="itemText">
             <p>In stock</p>
-            <h6>Oraimo mp4 Bluetoooth player</h6>
+            <h6>3D led Digital Wall Clock</h6>
           </Container>
           <Container fluid id="review">
             <Container id="star">
@@ -140,18 +149,23 @@ export const PopularViews = () => {
               <i className="fa-solid fa-star"></i>
               <i className="fa-solid fa-star"></i>
             </Container>
-            <p>37 reviews</p>
+            <p>50 reviews</p>
           </Container>
           <Container id="popularPriceDiv">
-            <h3>$29.99</h3>
+            <h3>$65.99</h3>
           </Container>
         </Container>
         {/* Third Item */}
         <Container id="item">
-          <img src={laptopImage} alt="Laptop" />
+          <Container fluid id="freeDeliveryDiv">
+            <Container id="innerFreeDelivery">
+              <h6>Free Delivery</h6>
+            </Container>
+          </Container>
+          <img src={clipperImage} alt="clippers" />
           <Container fluid id="itemText">
             <p>In stock</p>
-            <h6>11th Generation HP Laptop corei7</h6>
+            <h6>Deluxe Travel kit trimmer for Men</h6>
           </Container>
           <Container fluid id="review">
             <Container id="star">
@@ -161,18 +175,18 @@ export const PopularViews = () => {
               <i className="fa-solid fa-star"></i>
               <i className="fa-solid fa-star"></i>
             </Container>
-            <p>476 reviews</p>
+            <p>60 reviews</p>
           </Container>
           <Container id="popularPriceDiv">
-            <h3>$376.99</h3>
+            <h3>$26.99</h3>
           </Container>
         </Container>
         {/* Fourth Item */}
         <Container id="item">
-          <img src={redHeadphonesImage} alt="Headphones" />
+          <img src={fridgeImage} alt="Fridge" />
           <Container fluid id="itemText">
-            <p>In stock</p>
-            <h6>Oraimo Red Boys Headphones</h6>
+            <p>Few Units Left</p>
+            <h6>PanaSonic mini-sized refridgerator</h6>
           </Container>
           <Container fluid id="review">
             <Container id="star">
@@ -182,10 +196,10 @@ export const PopularViews = () => {
               <i className="fa-solid fa-star"></i>
               <i className="fa-solid fa-star"></i>
             </Container>
-            <p>177 reviews</p>
+            <p>30 reviews</p>
           </Container>
           <Container id="popularPriceDiv">
-            <h3>$134.86</h3>
+            <h3>$89.00</h3>
           </Container>
         </Container>
       </Container>
